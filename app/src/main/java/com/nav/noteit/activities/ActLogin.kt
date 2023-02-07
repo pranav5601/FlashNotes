7d342b8310f6d6387d14eb65421860871002d2be
pranav5601 <vachhanipranav61@gmail.com>
2023-02-07 13:06:17 -0500

    Init commit

--- app/src/main/java/com/nav/noteit/activities/ActLogin.kt
+++ app/src/main/java/com/nav/noteit/activities/ActLogin.kt
@@ -7,6 +7,7 @@ import com.nav.noteit.R
 class ActLogin : ActBase() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_login)
